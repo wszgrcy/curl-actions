b8d577b8655a4f7e016fc3f60979ac419dcc727e
chen <wszgrcy@gmail.com>
2025-05-05 17:24:02 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,36 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
 
-/**
- * The main function for the action.
- *
- * @returns Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const headers = core.getMultilineInput('headers')
+    const body = core.getInput('body')
+    const method = core.getInput('method')
+    const url = core.getInput('url')
+    core.info(headers.join('\n'))
+    core.info(body)
+    core.info(method)
+    core.info(url)
+    let headersList = headers.reduce(
+      (obj, item) => {
+        let index = item.indexOf(':')
+        let key = item.slice(0, index)
+        let value = item.slice(index + 1)
+        obj[key] = value
+        return obj
+      },
+      {} as Record<string, any>
+    )
+    core.info(JSON.stringify(headersList))
+    let bodyContent = JSON.stringify(body)
+    core.info(bodyContent)
+    let response = await fetch(url, {
+      method: method,
+      body: bodyContent,
+      headers: headersList
+    })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    let data = await response.text()
+    console.log(data)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
